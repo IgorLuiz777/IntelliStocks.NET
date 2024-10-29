94999da287af6302305c419930d34b88cfb81112
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-29 11:06:56 -0300

    feat: adding ML.NET

---
+++ INTELLISTOCKS.ML/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.ML;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddPredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput>()
+    .FromFile("MLModel.mlnet");
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "IntelliStocks", Description = "Docs for my ML API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = "swagger";
+});
+
+app.MapPost("/predict",
+    async (PredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput> predictionEnginePool, MLModel.ModelInput input) =>
+        await Task.FromResult(predictionEnginePool.Predict(input)));
+
+app.Run();
